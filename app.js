f68520259f97e5baf081ccfa23d8558cfd2ee462
kalyan <kalyanlee3@gmail.com>
2024-10-23 11:29:23 +0530

    enable cors

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const transactionRoutes = require('./routes/transactionRoutes');
 const { sequelize } = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
+
+// Enable CORS
+app.use(cors());
 
 app.use('', authRoutes);
 app.use('', transactionRoutes);
